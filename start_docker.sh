67a9a915cdf63f5ad06e990d1ead861fbae64ef5
Michael Ashe <mickjohnashe@hotmail.com>
2020-01-28 19:05:20 +0000

    Fxing nginx on docker for react router

--- start_docker.sh
+++ start_docker.sh
@@ -1,5 +1,6 @@
 docker run --name websocket-games \
     --rm \
-    -v $(pwd)/frontend_projects/homepage/build:/usr/share/nginx/html:ro \
+    -v $(pwd)/frontend/frontend/build:/usr/share/nginx/html:ro \
+    -v $(pwd)/frontend/nginx.conf:/etc/nginx/nginx.conf:ro \
     -p 80:80 \
     -d nginx
