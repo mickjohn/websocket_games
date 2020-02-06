36f4b854c6ac6a367a5e9a589d6a6340caa55a64
Michael Ashe <mickjohnashe@hotmail.com>
2020-02-06 19:39:16 +0000

    Updated main.py
    Adding start function for starting the websocket server, adding listen
    argument to specify what to listen on

--- websocketgames/main.py
+++ websocketgames/main.py
@@ -6,28 +6,27 @@ import argparse
 
 from websocketgames.server import WebsocketServer
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--port')
-args = parser.parse_args()
 
-# Configure Logger
-handler = logging.StreamHandler()
-logger = logging.getLogger('websocketgames')
-logger.addHandler(handler)
-fmt = '%(asctime)s %(name)s:%(module)s %(message)s'
-coloredlogs.install(logger=logger, level='DEBUG', fmt=fmt)
+async def start_server(address, port):
+    logger.info(f'Starting server on  {address}:{port}')
+    server = WebsocketServer()
+    return websockets.serve(server.handle_message, "0.0.0.0", args.port)
 
-if args.port:
-    port = args.port
-else:
-    port = 8080
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', default=8080)
+    parser.add_argument('--listen', default="0.0.0.0")
+    args = parser.parse_args()
 
-logger.info(f'Starting server on port {port}')
-server = WebsocketServer()
-start_server = websockets.serve(server.handle_message, "0.0.0.0", port)
+    # Configure Logger
+    handler = logging.StreamHandler()
+    logger = logging.getLogger('websocketgames')
+    logger.addHandler(handler)
+    fmt = '%(asctime)s %(name)s:%(module)s %(message)s'
+    coloredlogs.install(logger=logger, level='DEBUG', fmt=fmt)
 
-try:
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
-except KeyboardInterrupt:
-    logger.info("Shutting down server")
+    try:
+        asyncio.get_event_loop().run_until_complete(start_server(args.listen, args.port))
+        asyncio.get_event_loop().run_forever()
+    except KeyboardInterrupt:
+        logger.info("Shutting down server")
